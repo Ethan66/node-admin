0ca39af3f08a2f5ec363c0c85c866db919b2aecf
zc <zhuangcheng@ldxinyong.com>
2019-07-25 20:56:10 +0800

    update

--- appExample.js
+++ appExample.js
@@ -122,13 +122,14 @@ app.keys = ['some secret hurr']; // cookie的签名
  
 const CONFIG = {
   maxAge: 30000, // 设置过期时间，当过了30秒后session过期了,获取的就是undefined
-  autoCommit: true,
-  overwrite: true,
+  autoCommit: true, // 自动提交到响应头,后面所有接口request headers里都有Cookie属性
+  overwrite: true, // 是否允许重写
   httpOnly: true, // 只有服务器端可以获取cookie
   signed: true, // 默认签名
   rolling: false, // 每次访问重新设置过期时间，比如过期时间是30秒，过了20秒后又获取，所以又重新设置了session
   renew: true, // 过期时间30秒，快要过期了重新设置了session
 };
+app.use(session(CONFIG, app));
 router.get('/setSession', (ctx, next) => { // 先访问这个路由
   ctx.session.userInfo = '张三'
   ctx.body = '设置session成功'
