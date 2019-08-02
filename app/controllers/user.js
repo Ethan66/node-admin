22f0a5e7ec0360bda2241ec7d3481d552586e668
zc <zhuangcheng@ldxinyong.com>
2019-08-02 15:42:05 +0800

    用户管理修改用户的同时修改role表的userIdList

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,6 +2,7 @@ const md5 = require('md5')
 const passport = require('./../utils/passpot')
 const user_col = require('./../models/user')
 const menu_col = require('./../models/menu')
+const role_col = require('./../models/role')
 const password_col = require('./../models/password')
 
 // 注册
@@ -87,7 +88,17 @@ const getUser = async (ctx, next) => {
   ctx.success({ data: { list: result } })
 }
 
-const menu = async (ctx, next) => {
+// 获取授权菜单按钮（批量多个条件查询）
+const getUserAuthMenu = async (ctx, next) => {
+  let { userId } = ctx.request.body
+  let userObj = await user_col.findOne({ userId })
+  let roleObj = await role_col.findOne({ roleId: userObj.roleId })
+  let searchIds = Array.prototype.concat.call(roleObj.menuIdList, roleObj.btnIdList)
+  let result = await menu_col.find({ $or: [{ id: { $in: searchIds } }]})
+  ctx.success({ data: { list: result } })
+}
+
+const getAllMenu = async (ctx, next) => {
   let { menuName, status } = ctx.request.body
   let search = JSON.parse(JSON.stringify({ menuName, status }))
   search.delete = 0
@@ -121,18 +132,30 @@ const loginOut = async (ctx, next) => {
 
 // 编辑用户信息
 const modifyUserInfo = async (ctx, next) => {
-  let { id, account, name, password, status, operator } = ctx.request.body
+  let { id, account, name, password, status, operator, roleId } = ctx.request.body
   if (!account) {
     ctx.throw(400, '缺少参数')
   }
-  let result = await user_col.find({ user: account })
-  if (result.length > 0 && result[0].userId !== id) {
+  let result = await user_col.findOne({ user: account })
+  let oldRoleId = result.roleId
+  if (result && result.userId !== id) {
     return ctx.success({ code: '000001', msg: '账号已存在' })
   }
-  let search = { user: account, userName: name, status, operator }
-  if (!account) delete search.user
-  if (!name) delete search.userName
-  console.log(id, search)
+  let search = JSON.parse(JSON.stringify({ user: account, userName: name, status, operator, roleId }))
+  if (roleId !== oldRoleId) {
+    let roleObj = await role_col.findOne({ roleId: oldRoleId })
+    let { userIdList } = roleObj
+    let index = userIdList.indexOf(id)
+    if (index !== -1) {
+      userIdList.splice(index, 1)
+    }
+    await role_col.updateOne({ roleId: oldRoleId }, { $set: { userIdList } })
+    let newRoleObj = await role_col.findOne({ roleId })
+    search.roleName = newRoleObj.roleName
+    let newUserIdList = newRoleObj.userIdList
+    newUserIdList.push(id)
+    await role_col.updateOne({ roleId }, { $set: { userIdList: newUserIdList } })
+  }
   await user_col.update({ userId: id }, { $set: search }, () => {
     if (password) return false
     ctx.success()
@@ -144,7 +167,8 @@ const modifyUserInfo = async (ctx, next) => {
 
 // 新增用户
 const addUser = async (ctx, next) => {
-  const { account, name, password, operator } = ctx.request.body
+  const { account, name, password, operator, roleId } = ctx.request.body
+  let roleName = ''
   if (!account || !name || !password) {
     ctx.throw(400, '缺少参数')
   }
@@ -154,8 +178,15 @@ const addUser = async (ctx, next) => {
     return ctx.success({ msg: '账号已存在', code: '000001' })
   }
   const userId = userLength + 1
+  if (roleId) {
+    let result = await role_col.findOne({ roleId })
+    roleName = result.roleName
+    let userIdList = result.userIdList
+    userIdList.push(userId)
+    await role_col.updateOne({ roleId }, { $set: { userIdList } })
+  }
   const newUser = await user_col.create({
-    userId, user: account, userName: name, operator, createTime: ctx.formatDate()
+    userId, user: account, userName: name, operator, roleId, roleName, createTime: ctx.formatDate()
   })
   if (newUser) {
     const result = await password_col.create({ userId, password })
@@ -171,6 +202,14 @@ const deleteUser = async (ctx, next) => {
   if (id.includes(1)) {
     return ctx.success({ msg: '超级管理员不能删除', code: '000001' })
   }
+  for(let i = 0; i < id.length; i++) {
+    let result = await user_col.findOne({ userId: id[i] })
+    let roleObj = await role_col.findOne({ roleId: result.roleId })
+    let { userIdList } = roleObj
+    let index = userIdList.indexOf(id[i])
+    index !== -1 && userIdList.splice(index, 1)
+    await role_col.updateOne({ roleId: roleObj.roleId }, { $set: { userIdList } })
+  }
   await user_col.remove({ userId: { $in: id } })
   await password_col.remove({ userId: { $in: id } }, () => {
     ctx.success()
@@ -178,5 +217,5 @@ const deleteUser = async (ctx, next) => {
 }
 
 module.exports = {
-  register, login, menu, field, loginOut, modifyPassword, getUser, modifyUserInfo, deleteUser, addUser
+  register, login, getUserAuthMenu, getAllMenu, field, loginOut, modifyPassword, getUser, modifyUserInfo, deleteUser, addUser
 }
